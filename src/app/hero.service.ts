827055e85c1c82802e5e91d907cc2bed9b48b659
jpriesten <jepriesten@gmail.com>
2019-01-09 13:04:11 +0100

    First commit

---
+++ src/app/hero.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    // Send a message after heroes have been fetched
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+
+}
---
+++ src/app/heroe-detail/heroe-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroe-detail',
+  templateUrl: './heroe-detail.component.html',
+  styleUrls: ['./heroe-detail.component.css']
+})
+export class HeroeDetailComponent implements OnInit {
+  @Input() hero: Hero;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
